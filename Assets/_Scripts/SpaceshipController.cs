a8d0573ff0f662ccb9d8f096742c5f67609de3ef
NahoyGames <yohan.guyomard@gmail.com>
2019-03-23 16:40:21 -0700

    Initial Commit
    - Spaceship kinda
    - Terrain kinda

---
+++ Assets/_Scripts/SpaceshipController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SpaceshipController : MonoBehaviour
+{
+
+
+    [SerializeField] private string rollInput;
+    [SerializeField] private string yawInput;
+    [SerializeField] private string pitchInput;
+
+
+    [SerializeField] private float rollSpeed;
+    [SerializeField] private float yawSpeed;
+    [SerializeField] private float pitchSpeed;
+
+    private Rigidbody rb;
+
+    private Vector3 vel;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float roll = -Input.GetAxis(rollInput) * rollSpeed;
+        float yaw = Input.GetAxis(yawInput) * yawSpeed;
+        float pitch = Input.GetAxis(pitchInput) * pitchSpeed;
+
+        vel.z = yaw;
+        vel.y = roll - (yaw / 2  * yawSpeed);
+        vel.x = pitch;
+
+        rb.AddRelativeTorque(vel);
+
+        rb.AddRelativeForce(Vector3.up * 2, ForceMode.Impulse);
+
+        Debug.Log(transform.localEulerAngles.z);
+    }
+
+}
