d5067d94ad5cf7ca9bf9bf06ae9561a9d4c6ec41
Seth Moos <sethmoosepic@gmail.com>
2019-03-23 23:16:49 -0700

    Updated Movement & Fixed Player Size
    Movement is now realistic, only jumping is left for me to fix

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -10,6 +10,8 @@ public class Player : MonoBehaviour
     public float verticalSpeed = 2.0f;
     public float jumpForce = 2.0f;
     public float sideSpeed = 2.0f;
+    public float isSprinting = 1.0f;
+    public float multiplier = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,15 +21,21 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float translation = Input.GetAxis("Vertical") * Time.deltaTime * Speed;
-       float side = Input.GetAxis("Horizontal") * Time.deltaTime * sideSpeed;
+        float translation = Input.GetAxis("Vertical") * Time.deltaTime * Speed * isSprinting;
+        float side = Input.GetAxis("Horizontal") * Time.deltaTime * sideSpeed * isSprinting * 0.9f;
         float h = horizontalSpeed * Input.GetAxis("Mouse X") * Time.deltaTime;
         float v = verticalSpeed * Input.GetAxis("Mouse Y") * Time.deltaTime;
         transform.Translate(side, 0, translation);
         transform.Rotate(-v, h, 0);
+        if (Input.GetButton("Sprint")) {
+            isSprinting = multiplier;
+         }
+        else {
+            isSprinting = 1;
+        }
         if (rb.velocity.sqrMagnitude < 0.1) {
             float j = jumpForce * Input.GetAxis("Jump");
-            transform.Translate(0, j, 0);
+            rb.AddForce(0, j, 0);
         }
     } 
 }
