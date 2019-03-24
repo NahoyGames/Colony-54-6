3b39d8858513121592078bbd010a16a72986ca65
NahoyGames <yohan.guyomard@gmail.com>
2019-03-24 01:57:44 -0700

    Improvements to the map + destructible

---
+++ Assets/_Scripts/Destructible.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour
+{
+    public float maxHealth;
+    public int minDrops, maxDrops;
+
+    public GameObject deathParticle;
+    public GameObject dropObject;
+
+    private float health;
+
+
+    public void Damage(float amount)
+    {
+        this.health -= amount;
+
+        if (health <= 0)
+        {
+            Kill();
+        }
+    }
+
+
+    public void Kill()
+    {
+        Instantiate(deathParticle, transform.position, Quaternion.identity);
+
+
+        int dropCount = Random.Range(minDrops, maxDrops);
+
+
+        for (int i = 0; i < dropCount; i++)
+        {
+            Instantiate(dropObject);
+        }
+
+        Destroy(this.gameObject);
+    }
+}
