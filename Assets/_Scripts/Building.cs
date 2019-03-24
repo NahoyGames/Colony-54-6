ab727619780a7eba445d004c62f2ad1deb087471
NahoyGames <yohan.guyomard@gmail.com>
2019-03-24 05:15:22 -0700

    Building basics + balancing

---
+++ Assets/_Scripts/Building.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    [SerializeField] private GameObject wall;
+    [SerializeField] private Vector3 gridSize;
+
+    private Camera cam;
+
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+
+    private void Update()
+    {
+        // The world position of the cursor
+        Vector3 cursorPos = cam.ScreenToWorldPoint(Vector3.forward * cam.nearClipPlane);
+
+        Vector3 gridCursorPos = new Vector3(cursorPos.x - (cursorPos.x % gridSize.x),
+                                            cursorPos.x - (cursorPos.x % gridSize.x),
+                                            cursorPos.x - (cursorPos.x % gridSize.x));
+
+    }
+}
